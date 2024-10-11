7c957bf1bdee4e81ffa6c4432739c1de80468eb7
naimuddin94 <developernaimuddin@gmail.com>
2024-10-11 10:06:38 +0600

    set hero bg image

--- src/app/(withComonLayout)/page.tsx
+++ src/app/(withComonLayout)/page.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
-
 import { subtitle, title } from "@/src/components/primitives";
 
 export default async function Home() {
   return (
     <section
-      className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 bg-cover bg-center"
-      style={{ backgroundImage: 'url("/path-to-your-garden-bg-image.jpg")' }}
+      className="relative flex min-h-[calc(100vh-64px)] flex-col items-center justify-center gap-4 py-8 md:py-10 bg-cover bg-center"
+      style={{ backgroundImage: 'url("/hero_bg.jpg")' }}
     >
-      <div className="inline-block max-w-xl text-center justify-center">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-50 z-0" />
+
+      <div className="relative inline-block max-w-xl text-center justify-center z-10">
         <span className={title()}>Grow&nbsp;</span>
         <span className={title({ color: "green" })}>your dream&nbsp;</span>
         <br />
@@ -23,7 +25,7 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="flex gap-3">
+      <div className="relative flex gap-3 z-10">
         <Link
           className="bg-green-600 px-8 py-3 rounded-full no-underline text-white hover:brightness-90"
           href="/dashboard"
